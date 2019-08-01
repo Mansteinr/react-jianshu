4616411e6f4b3870c40a542bd889ddf3553bbeb0
MINIVISION\zhoumingye <zhoumingye@minivision.cn>
2019-08-01 15:58:39 +0800

    header组件使用iconfont

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './index.styl'
+// import '../../common/iconfont/iconfont.styl'
 
 export default class Header extends Component {
   render() {
@@ -9,13 +10,20 @@ export default class Header extends Component {
         <div className="nav-item left active">首页</div>
         <div className="nav-item left">下载APP</div>
         <div className="nav-item right">登陆</div>
-        <div className="nav-item right">Aa</div>
-        <div className="nav-search">
-          <input placerholder="搜索"/>
+        <div className="nav-item right">
+          <i className="iconfont icon-Aa"></i>
+        </div>
+        <div className="search-wrapper">
+          <div className="nav-search">
+            <input placeholder="搜索"/>
+            <i className="iconfont icon-fangdajing"></i>
+          </div>
         </div>
       </div>
       <div className="addtion">
-        <div className="button writting">写文章</div>
+        <div className="button writting">
+        <i className="iconfont icon-qianbi"></i>
+          写文章</div>
         <div className="button reg">注册</div>
       </div>  
     </div>)
--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './common/stylus/reset.styl'
+import './common/iconfont/iconfont.styl'
 ReactDOM.render(<App />, document.getElementById('root'))
