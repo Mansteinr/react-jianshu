17f4c15328a88a30180a8be43cc0dd55c4046d38
MINIVISION\zhoumingye <zhoumingye@minivision.cn>
2019-08-01 15:25:06 +0800

    header组件开发

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,7 +5,7 @@ export default class Header extends Component {
   render() {
     return (<div className="header-wrapper">
       <a href="/"></a>
-      <div class="nav">
+      <div className="nav">
         <div className="nav-item left active">首页</div>
         <div className="nav-item left">下载APP</div>
         <div className="nav-item right">登陆</div>
@@ -14,6 +14,10 @@ export default class Header extends Component {
           <input placerholder="搜索"/>
         </div>
       </div>
+      <div className="addtion">
+        <div className="button writting">写文章</div>
+        <div className="button reg">注册</div>
+      </div>  
     </div>)
   }
 }
