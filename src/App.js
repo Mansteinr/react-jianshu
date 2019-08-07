88eedd69cd187fc3802222ef01a36b3d1f52368e
MINIVISION\zhoumingye <zhoumingye@minivision.cn>
2019-08-07 14:14:23 +0800

    react路由的引入

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import Header from './components/header'
 import store from './store'
+import { BrowserRouter, Route } from 'react-router-dom'
 
 export default class App extends Component{
   render() {
     return (
       // Provider把store里面存储的数据提供给他内部的组件
       <Provider store={store}>
-        <Header/>
+        <div>
+          <Header/>
+          <BrowserRouter>
+          <div>
+            <Route path='/' exact render={ () => <div>home</div> }></Route>
+            <Route path='/detail' exact render={ () => <div>detail</div> }></Route>
+          </div>
+          </BrowserRouter>
+        </div>
       </Provider>
     )
   }
